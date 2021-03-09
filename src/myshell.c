d8f07ea2891ea341d3d04c309ebb322478e65bbc
malmal200 <malachybyrne1@gmail.com>
2021-03-09 17:47:01 +0000

    added cd command

--- src/myshell.c
+++ src/myshell.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <ctype.h>
 #include "path.h"
 
 #define MAX_COMM_SIZE 256
@@ -11,6 +12,25 @@
 #define PATH_COLOUR "\033[34m"
 #define CLEAR_FORMAT "\033[0m"
 
+void cd(char* loc) {
+    chdir(loc);
+}
+
+char* pwd() {
+    char* dir = (char*)calloc(MAX_COMM_SIZE, sizeof(char));
+    getcwd(dir, (size_t)MAX_COMM_SIZE);
+    return dir;
+}
+
+void trim(char * s) { // Taken from jkramer's answer to https://stackoverflow.com/questions/122616/how-do-i-trim-leading-trailing-whitespace-in-a-standard-way
+    char * p = s;
+    int l = strlen(p);
+
+    while(isspace(p[l - 1])) p[--l] = 0;
+    while(* p && isspace(* p)) ++p, --l;
+
+    memmove(s, p, l + 1);
+}
 
 int test_comm(char* comm) {
     struct dirent *curr_dir;
@@ -32,6 +52,18 @@ int handle_command(char* command_line, char* dir) {
         free(command);
         return -1;
     }
+    if (strcmp(command, "cd") == 0) {
+        if (strcmp(command, command_line) != 0) {
+            char* loc = (char*)calloc(MAX_COMM_SIZE, sizeof(char));
+            int i = strlen(command);
+            while (++i < strlen(command_line))
+                strncat(loc, &command_line[i], 1);
+            cd(loc);
+            return 0;
+        }
+        printf("%s\n", pwd());
+        return 0;
+    }
     if(test_comm(command)) {
         char* curr_comm = (char*)calloc(MAX_COMM_SIZE, sizeof(char));
         strcat(curr_comm, PATH);
@@ -48,14 +80,20 @@ int handle_command(char* command_line, char* dir) {
 }
 
 int main(int argc, char* argv[]) {
+    char* shell_path = (char*)calloc(MAX_COMM_SIZE, sizeof(char));
+    strncat(shell_path, "myshell=");
+    strncat(shell_path, PATH);
+    strncat(shell_path, "/myshell");
+    putenv(shell_path);
     char directory[PATH_MAX];
     getcwd(directory, sizeof(directory));
     if (argc == 1) {
         char* command_line = malloc(MAX_COMM_SIZE * sizeof(char));
         int comm_res = 0;
         while (comm_res != -1) {
-            printf("%s%s%s%s$ ", BOLD, PATH_COLOUR, PATH, CLEAR_FORMAT);
+            printf("%s%s%s%s$ ", BOLD, PATH_COLOUR, pwd(), CLEAR_FORMAT);
             fgets(command_line, MAX_COMM_SIZE, stdin);
+            trim(command_line);
             comm_res = handle_command(command_line, directory);
         }
         free(command_line);
